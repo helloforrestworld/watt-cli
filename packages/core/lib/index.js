61f8f12f5fe5ae8ce471989c69664bccd692f2af
helloforrestworld <helloforrestworld@163.com>
2021-06-30 17:23:02 +0800

    feat: add yarg package

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 
-console.log('hello watt cli')
+const yargs = require("yargs/yargs");
+const hideBin = yargs.hideBin;
+const arg = hideBin(process.argv);
+const cli = yargs(arg);
+
+cli
+  .usage("Usage: watt [command] <options>")
+  .demandCommand(1, "A command is required. Pass --help to see all available commands and options.")
+  .strict()
+  .wrap(cli.terminalWidth())
+  .locale("en")
+  .alias("v", "version")
+  .alias("h", "help")
+  .options({
+      debug: {
+          type: 'boolean',
+          describe: 'Bootstrap debug mode',
+          alias: 'd'
+      }
+  })
+  .option('registry', {
+      type: 'string',
+      describe: 'Define global registry',
+      alias: 'r'
+  })
+  .group(['debug'], 'Dev Options:')
+  .epilogue("you own footer description").argv;
